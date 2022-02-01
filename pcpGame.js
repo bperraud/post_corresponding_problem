3a52ce800040c07d8191a91419aa75ceb012f12f
adchejjari <a.chejjari@gmail.com>
2022-02-01 23:11:42 +0100

    add Game class

---
+++ pcpGame.js
@@ -0,0 +1,20 @@
+import Instance from './post.js'
+import Pair from './post.js'
+
+
+class Game{
+    constructor(_instance){
+        this.instance = _instance;
+        this.solution = null;
+    }
+
+    play(){
+
+    }
+
+    gameLoop(){
+        while (!this.solution.isWon()){
+            play
+        }
+    }
+}
--- post.js
+++ post.js
@@ -6,9 +6,17 @@ class Instance {
     this.pairs = pairs; // liste des blocs (ordre pas important + pas de duplication)
     }
 
+    getSize(){
+        return this.size;
+    }
+
+    getPairs(){
+        return this.pairs;
+    }
+
     // 1 si l'instance possède une solution, 0 sinon, -1 si on ne sait pas
     isSolvable(){
-        return 1;
+        
     }
 }
 
@@ -17,37 +25,58 @@ class Pair {
     constructor(top, bottom){
     this.top = top;     // string (ou mot) du dessus
     this.bottom = bottom;   // string du dessous
-    }  
+    }
+
+    getTop(){
+        return this.top;
+    }
+
+    getBottom(){
+        return this.bottom;
+    }
 }
     
+
 class Solution {
     constructor(pairs){
         this.pairs = pairs; // liste des blocs (ordre important + duplication possible)
     }
 
     // ajoute à l'indice index dans la liste
-    add(index) {
-
+    add(index, pair) {
+        let tempPair = this.pairs[index];
+        this.pairs[index] = pair;
+        this.pairs.push(tempPair);        
     }
 
     //ajoute a la fin de la liste
-    push() {
-
+    push(pair) {    // 
+        this.pairs.push(pair);
     }
 
     //enchange un bloc à l'indice index avec un autre
-    swap(index, pair){
-
+    swap(index1, index2){
+        let pair1 = this.pairs[index1];
+        let pair2 = this.pairs[index2];
+        this.pairs[index1] = pair2;
+        this.pairs[index2] = pair1;
     }
 
     //supprime un bloc à l'indice index
-    delete(index){
+    delete(index){ // On a le droit de supprimer ?
 
     }
 
     // 1 si la liste this.pairs forme une solution du problème de Post, 0 sinon
     isWon() {
-        return 1;
+        let top="";
+        let bottom="";
+        for (i=0;i<this.size;i++){
+            let currentPair = this.pairs[i];
+            top += currentPair.getTop();
+            bottom += currentPair.getBottom();
+        }
+        return top == bottom;
     }
 }
     
