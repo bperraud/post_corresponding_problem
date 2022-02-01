fde60b3cc47d70ac5e20177a5f2175d633502109
Benjamin <bperraud@ulb.be>
2022-02-01 19:46:08 +0100

    up

--- post.js
+++ post.js
@@ -1,12 +1,14 @@
 class Instance {
-    constructor(size, width, pairs) {
+    constructor(size, width, alphabet, pairs) {
     this.size = size;
     this.width = width;
+    this.alphabet = alphabet;
     this.pairs = pairs;
     }
-      
 
-
+    isSolvable(){
+        return 1;
+    }
 }
 
 class Pair {
@@ -17,6 +19,25 @@ class Pair {
 }
     
 class Solution {
+    constructor(pairs){
+        this.pairs = pairs;
+    }
+
+    //ajoute a la fin de la liste
+    push() {
+
+    }
+
+    //enchange un bloc à l'indice index avec un autre
+    swap(index, pair){
+
+    }
+
+    //supprime un bloc à l'indice index
+    delete(index){
+
+    }
+
     isWon() {
         return 1;
     }
