a3e59ffc62be9058a6eb380f7c4d30355a7ad2a8
Benjamin <bperraud@ulb.be>
2022-02-11 02:06:28 +0100

    Pcp_bloc + Pcp_instance declaration

---
+++ src/PCP_solve/pcp_solving.cpp
@@ -0,0 +1,28 @@
+#include "pcp_solving.h"
+
+
+
+using namespace std;
+
+bool Pcp_bloc::has_prefix()
+{
+	return strncmp(_top, _bottom,  ABS( _width - top_less_bottom()));
+}
+
+Pcp_bloc::Pcp_bloc(const char *top, const char *bottom){
+	_top = top;
+	_bottom = bottom;
+	_width = MAX(strlen(top), strlen(bottom));
+}
+
+int main(void)
+{
+	Pcp_bloc* bloc = new Pcp_bloc((char*)"100", (char*)"101");
+
+	cout << bloc->has_prefix() << endl;
+}
+
+
+
+
+
