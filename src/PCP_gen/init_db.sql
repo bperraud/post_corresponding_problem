6ea7e6973c7f4134f4732ceded1fc8d6c835efcb
loleloup <loic.leloup@ulb.ac.be>
2022-02-11 16:14:34 +0100

    fixed privileges granting for PCPadmin

--- src/PCP_gen/init_db.sql
+++ src/PCP_gen/init_db.sql
@@ -2,7 +2,7 @@ use mysql;
 create user if not exists 'PCPadmin' identified by 'AXaHUKc])n2D%t*"T6Ve';  #unified password for everyone
 CREATE DATABASE if not exists PCP;
 
-grant all privileges on PCP to 'PCPadmin'; #all privileges on PCP tables for admin
+grant all privileges on PCP.* to 'PCPadmin'; #all privileges on PCP tables for admin
 
 use PCP;
 
