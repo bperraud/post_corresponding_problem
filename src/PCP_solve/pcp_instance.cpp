cf84c4f1a7eca50e9e05841050dc0351064ea050
loleloup <loic.leloup@ulb.ac.be>
2022-02-14 22:20:51 +0100

    added write to db

--- src/PCP_solve/pcp_instance.cpp
+++ src/PCP_solve/pcp_instance.cpp
@@ -1,10 +1,14 @@
 #include "pcp_instance.h"
 
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/statement.h>
 
-Pcp_instance::Pcp_instance(std::initializer_list<std::string> blocs) : _lenght(blocs.size()/2), _instance(blocs.size()/2, Pcp_bloc())
+
+Pcp_instance::Pcp_instance(std::initializer_list<std::string> blocs) : _length(blocs.size()/2), _instance(blocs.size()/2, Pcp_bloc())
 {
 	auto bloc = blocs.begin();
-	for (std::size_t i = 0; i < _lenght; i++)
+	for (std::size_t i = 0; i < _length; i++)
 	{
 		_instance[i] = Pcp_bloc(*bloc, *(bloc+1));
 		bloc += 2;
@@ -14,7 +18,7 @@ Pcp_instance::Pcp_instance(std::initializer_list<std::string> blocs) : _lenght(b
 
 bool Pcp_instance::prefix_filter()
 {
-	for (int i = 0; i < _lenght ; i++)
+	for (int i = 0; i < _length ; i++)
 	{
 		if (_instance[i].has_prefix())
 			return (false);
@@ -25,11 +29,11 @@ bool Pcp_instance::prefix_filter()
 
 bool Pcp_instance::length_balance_filter()
 {
-	for (int i = 0; i < _lenght ; i++)
+	for (int i = 0; i < _length ; i++)
 	{
 		if (_instance[i].has_prefix())																		 // le bloc i peut être placé en premier
 		{
-			for (int j = 0; j < _lenght; j++)
+			for (int j = 0; j < _length; j++)
 			{
 				if ( j != i && _instance[j].top_less_bottom_len() + _instance[i].top_less_bottom_len() <= 0) // il existe une autre pair qui contrebalance la longueur de la plus grande string
 					return (false);
@@ -42,11 +46,11 @@ bool Pcp_instance::length_balance_filter()
 
 bool Pcp_instance::element_balance_filter()
 {
-	for (int i = 0; i < _lenght ; i++)
+	for (int i = 0; i < _length ; i++)
 	{
 		if (_instance[i].has_prefix())																		 // le bloc i peut être placé en premier
 		{
-			for (int j = 0; j < _lenght; j++)
+			for (int j = 0; j < _length; j++)
 			{
 				if ( j != i && _instance[j].top_less_bottom_ones() + _instance[i].top_less_bottom_ones() <= 0) // il existe une autre pair qui contrebalance le nombre de 1
 					return (false);
@@ -54,4 +58,43 @@ bool Pcp_instance::element_balance_filter()
 		}
 	}
 	return (true);
+}
+
+
+std::string Pcp_instance::stringify() {
+    std::string res;
+    for (int i = 0; i < _length; i++){
+        Pcp_bloc bloc = _instance[i];
+        res += bloc.get_top() + "," + bloc.get_bottom() + ",";
+    }
+    res.pop_back(); //removes last ','
+    return res;
+}
+
+
+void Pcp_instance::write_instance() {
+    try {
+        sql::Driver *driver;
+        sql::Connection *con;
+        sql::Statement *stmt;
+
+        /* Create a connection */
+        driver = get_driver_instance();
+        con = driver->connect("localhost", "PCPadmin", "AXaHUKc])n2D%t*\"T6Ve");
+        con->setSchema("PCP");
+
+        stmt = con->createStatement();
+        stmt->execute("INSERT INTO instances(pairs, sol_len) VALUES ('" + stringify() + "', '" + std::to_string(sol_len)+ "')");
+
+        delete stmt;
+        delete con;
+    }
+    catch (sql::SQLException &e) {
+        std::cout << "# ERR: SQLException in " << __FILE__;
+        std::cout << "(" << __FUNCTION__ << ") on line "
+             << __LINE__ << std::endl;
+        std::cout << "# ERR: " << e.what();
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+    }
 }
--- src/PCP_solve/pcp_instance.h
+++ src/PCP_solve/pcp_instance.h
@@ -6,16 +6,19 @@
 
 class Pcp_instance{
 
-	unsigned int _lenght;				// nombre de bloc
+	unsigned int _length;				// nombre de bloc
 	unsigned int _width; 				// longueur de la plus grande string
 	std::vector<Pcp_bloc> _instance;	// liste des blocs
+    unsigned int sol_len = 0;
 
 public:
-	Pcp_instance(unsigned int lenght, unsigned int width) : _lenght(lenght), _width(width) {}
+	Pcp_instance(unsigned int length, unsigned int width) : _length(length), _width(width) {}
 	Pcp_instance(std::initializer_list<std::string>);
 	bool prefix_filter();
 	bool length_balance_filter();
 	bool element_balance_filter();
+    std::string stringify();
+    void write_instance();
 
 };
 
