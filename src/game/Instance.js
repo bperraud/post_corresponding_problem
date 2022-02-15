04167d82b10a118cf43f3bb46ee6d1d336711626
loleloup <loic.leloup@ulb.ac.be>
2022-02-15 22:34:48 +0100

    added getter from db

--- src/game/Instance.js
+++ src/game/Instance.js
@@ -1,3 +1,6 @@
+import {getSession} from '@mysql/xdevapi';
+import Pair from './Pair.js';
+
 export default class Instance {
     constructor(size, width, pairs) {
     this.size = size;
@@ -16,6 +19,36 @@ export default class Instance {
     // 1 si l'instance possède une solution, 0 sinon, -1 si on ne sait pas
     isSolvable(){
         
+    }
+
+    async get_from_db() {
+        const config = {
+            password: 'AXaHUKc])n2D%t*\"T6Ve',
+            user: 'PCPadmin',
+            host: 'localhost',
+            schema: 'PCP'
+        };
+
+
+        await getSession(config)
+            .then(session => {
+                return session.sql('select * from instances order by rand()')
+                    .execute().then(session.close());
+            })
+            .then(result => {
+                let pairs = result.fetchOne()[1];
+
+                this.pairs = [];
+                pairs = pairs.split(",");
+
+                for (let i = 0; i < pairs.length / 2; i++) {
+                    this.pairs.push(new Pair(pairs[2 * i], pairs[2 * i + 1]))
+
+                }
+            }).catch(error => {
+                // Use `error` here without `throw`ing -- report it, put it in a log, etc.
+                console.log(error)
+            });
     }
 
     displayPairs(){
