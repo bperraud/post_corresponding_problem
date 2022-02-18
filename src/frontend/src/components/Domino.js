46aa7b1cab18ce1f82fc44d2908df81324402eb5
parhamoyan <parhamoyan@yahoo.com>
2022-02-18 17:10:23 +0100

    update Domino component

--- src/frontend/src/components/Domino.js
+++ src/frontend/src/components/Domino.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Domino = () => {
+const Domino = ({topText, bottomText}) => {
   return (
-    <div>
+    <button>
       <div class="dominoes-tile">
-        <div class="dominoes-tile__top"></div>
+        <div class="dominoes-tile__top">
+          <p>{topText}</p>
+        </div>
         <div class="dominoes-tile__bottom">
+          <p>{bottomText}</p>
         </div>
       </div>
-    </div>
-    
+    </button>
   )
+}
+
+Domino.defaultProps = {
+  topText: 'AB',
+  bottomText: 'BC'
+}
+
+Domino.prototype = {
+  topText: PropTypes.string,
+  bottomText: PropTypes.string
 }
 
 export default Domino
