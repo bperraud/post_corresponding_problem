b1b8f78cf21c1b696a2718c384b648dec86ee5a0
adchejjari <a.chejjari@gmail.com>
2022-02-20 11:31:32 +0100

    add header to reactapp

--- src/src/App.js
+++ src/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Domino from './components/Domino';
+import Navbar from './components/Navbar'
 
 function App() {
   const elements = [["AB", "CD"], ["EF", "GH"], ["IJ", "KL"]];
   return (
+
+    
+
     <div className="App">
+      <div className='Header'>
+      <Navbar/>
+      </div>
       <ul>
         {elements.map((item, index) => <Domino topText={item[0]} bottomText={item[1]}/>)}
       </ul>  
